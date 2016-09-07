ca9a6376c194ad87efb140570240a8c57d9416dc
juriszubkans <juris.zubkans@gmail.com>
2016-09-07 14:36:32 +0300

    fixed double call of http.login

--- admweb/src/app/component/login.ts
+++ admweb/src/app/component/login.ts
@@ -1,36 +1,35 @@
 import {Component} from "@angular/core";
 import {Router, ROUTER_DIRECTIVES} from "@angular/router";
 import {CORE_DIRECTIVES} from "@angular/common";
+import {Subscriber} from "rxjs/Rx";
 import {UserService} from "../service";
 
-const template = require('./login.html');
-
 @Component({
-    selector: 'login',
-    directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES],
-    template: template,
-    providers: [UserService]
+	selector: 'login',
+	directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES],
+	templateUrl: './login.html',
+	providers: [UserService]
 })
 export class Login {
-    private model = {username: null, password: null};
-    private loginFailed = false;
+	private model = {username: null, password: null};
+	private loginFailed = false;
 
-    constructor(private userService:UserService, private router:Router) {
-    }
+	constructor(private userService: UserService, private router: Router) {
+	}
 
-    public login(event:KeyboardEvent) {
-        this.loginFailed = false;
-        if (event !== undefined && event.keyCode !== 13) {
-            return;
-        }
+	public login(event: KeyboardEvent) {
+		this.loginFailed = false;
+		if (event !== undefined && event.keyCode !== 13) {
+			return;
+		}
+		let callbacks = Subscriber.create(
+			(data) => this.router.navigate(["home"]),
+			(err) => this.showLoginError()
+		);
+		this.userService.login(this.model.username, this.model.password, callbacks);
+	}
 
-        this.userService.login(this.model.username, this.model.password).subscribe(
-            (data) => this.router.navigate(["home"]),
-            (err) => this.showLoginError()
-        );
-    }
-
-    private showLoginError() {
-        this.loginFailed = true;
-    }
+	private showLoginError() {
+		this.loginFailed = true;
+	}
 }
--- admweb/src/app/service/user.service.ts
+++ admweb/src/app/service/user.service.ts
@@ -1,32 +1,41 @@
 import {Injectable} from "@angular/core";
-import {Http, Response} from "@angular/http";
-import {Observable} from "rxjs/Rx";
+import {Http} from "@angular/http";
+import {Observer} from "rxjs/Rx";
+import {empty} from "rxjs/Observer";
+
+export class User {
+	constructor(public isLoggedIn: boolean, username: String, permissions: String[]) {
+	}
+}
 
 @Injectable()
 export class UserService {
-    private user = {isLoggedIn: false, username: null, permissions: null};
+	private user = new User(false, null, null);
 
     constructor(public http:Http) {
     }
 
-    public login(username:String, password:String):Observable<Response> {
+	public login(username: String, password: String, callbacks: Observer<any> = empty): void {
         let loginObservable = this.http.post('/api/admin/login', {'name': username, 'password': password})
-            .map(res=> res.json());
-
-        loginObservable.subscribe(
-            (data) => this.makeLogin(data, username),
-            (err) => this.resetUser()
-        );
-
-        return loginObservable;
+			.map(res=> res.json())
+			.subscribe(
+				(data) => {
+					this.makeLogin(data, username);
+					callbacks.next(data);
+				},
+				(error) => {
+					this.resetUser();
+					callbacks.error(error);
+				}
+			);
     }
 
     private makeLogin(permissions:any, username:String):void {
-        this.user = {isLoggedIn: true, username: username, permissions: permissions};
+		this.user = new User(true, username, permissions);
     }
 
     private resetUser():void {
-        this.user = {isLoggedIn: false, username: null, permissions: null};
+		this.user = new User(false, null, null);
     }
 
     private isLoggedIn():boolean {
