5fe82f52c73413d61c173dbec8d1bf983337d3e0
juriszubkans <juris.zubkans@gmail.com>
2017-01-17 16:45:24 +0200

    still has errors on admin module routing, introduced arrays of Route[]

--- admweb/src/app/admin/admin.routes.ts
+++ admweb/src/app/admin/admin.routes.ts
@@ -1,19 +1,22 @@
-import {Routes} from "@angular/router";
+import {Routes, Route} from "@angular/router";
 import {AdminHomePage} from "./admin.home";
 import {AdminBasePage} from "./admin.page";
 import {AuthGuard} from "../service/auth.gurad";
 import {About} from "./about/about.component";
-import {USER_ROUTES} from "./user/user.routes";
+import {USER_MODULE_ROUTES} from "./user/user.routes";
+
+const GENERAL_ADMIN_ROUTES: Route[] = [
+	{path: '', redirectTo: 'home', pathMatch: 'full'},
+	{path: 'home', component: AdminHomePage},
+	{path: 'about', component: About}
+];
 
 export const ROUTES: Routes = [
 	{
 		path: 'admin',
 		component: AdminBasePage,
 		canActivate: [AuthGuard],
-		children: [
-			{path: '', redirectTo: 'home', pathMatch: 'full'},
-			{path: 'home', component: AdminHomePage},
-			{path: 'about', component: About}
-		].concat(USER_ROUTES)
+		children: GENERAL_ADMIN_ROUTES
+			.concat(USER_MODULE_ROUTES)
 	}
 ];
--- admweb/src/app/admin/user/user.routes.ts
+++ admweb/src/app/admin/user/user.routes.ts
@@ -1,10 +1,10 @@
-import {Routes} from "@angular/router";
+import {Route} from "@angular/router";
 import {Users} from "./user.list.component";
 import {User} from "./user.edit.component";
 import {Roles} from "./role.list.component";
 import {Role} from "./role.edit.component";
 
-export const USER_ROUTES: Routes = [
+export const USER_MODULE_ROUTES: Route[] = [
 	{path: 'users', component: Users},
 	{path: 'user/:id', component: User},
 	{path: 'user-roles', component: Roles},
